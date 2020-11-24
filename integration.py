e10b554c2c2d672fc8cecea79491f5fcd2b2d9d2
KRBM <kaspermuller@gmail.com>
2020-11-24 11:48:21 +0100

    Done with the example

--- integration.py
+++ integration.py
@@ -1,11 +1,6 @@
 import numpy as np
-from numpy import sin, pi
-
-
-def two_var(f, s):
-    def g(x):
-        return f(x, s)
-    return g
+from numpy import sin, pi, exp
+from tqdm import tqdm
 
 
 class IntegrationError(Exception):
@@ -32,17 +27,15 @@ class Integrate:
             lower, upper = upper, lower
             self.sign = -1
         number_of_points = (upper - lower) * precision
-        xs = list(np.linspace(lower, upper, int(number_of_points)))
+        xs = np.linspace(lower, upper, int(number_of_points))
         integral = 0
         super_sum = 0
         sub_sum = 0
-        for index in range(len(xs) - 1):
+        for index in tqdm(range(len(xs) - 1)):
             delta = xs[index + 1] - xs[index]
             try:
-                last = xs[index]
                 y1 = self.function(xs[index])
                 sub_area = y1 * delta
-                last = xs[index + 1]
                 y2 = self.function(xs[index + 1])
                 super_area = y2 * delta
 
@@ -51,7 +44,7 @@ class Integrate:
                 sub_sum += sub_area
                 super_sum += super_area
             except ZeroDivisionError:
-                print(f"\nAvoided pole at {last}\n")
+                print(f"\nAvoided pole")
 
         error = super_sum - sub_sum
         self.error = f"{integral - error} < integral < {integral + error}\n"
@@ -63,20 +56,18 @@ class Integrate:
         x_list, y_list = limit_list
         (a, b), (c, d) = x_list, y_list
         x_points, y_points = (b - a) * precision, (d - c) * precision
-        xs, ys = list(np.linspace(a, b, int(x_points))), list(np.linspace(c, d, int(y_points)))
+        xs, ys = np.linspace(a, b, int(x_points)), np.linspace(c, d, int(y_points))
         integral = 0
         sub_sum = 0
         super_sum = 0
-        for i in range(len(xs) - 1):
+        for i in tqdm(range(len(xs) - 1)):
             delta_x = xs[i + 1] - xs[i]
             for j in range(len(ys) - 1):
                 delta_y = ys[j + 1] - ys[j]
                 delta = delta_x * delta_y
                 try:
-                    last = (xs[i], ys[j])
                     f1 = self.function(xs[i], ys[j])
                     sub_area = f1 * delta
-                    last = (xs[i + 1], ys[j + 1])
                     f2 = self.function(xs[i + 1], ys[j + 1])
                     super_area = f2 * delta
 
@@ -85,7 +76,7 @@ class Integrate:
                     sub_sum += sub_area
                     super_sum += super_area
                 except ZeroDivisionError:
-                    print(f"\nAvoided pole at {last}\n")
+                    print(f"\nAvoided pole\n")
 
         error = super_sum - sub_sum
         self.error = f"{integral - error} < integral < {integral + error}\n"
@@ -94,15 +85,19 @@ class Integrate:
 
 if __name__ == "__main__":
 
-    def double(x, y):
-        return x * y**2
+    def double_gaussian(x, y):
+        return exp(-(x ** 2 + y ** 2))
 
-    def d(x):
-        return sin(x) / x
 
-    integrate = Integrate(double)
-    result = integrate.double_integral([[0, 3], [1, 2]])
-    print(result)
-    integrate = Integrate(d)
-    print(integrate.integral(1, -1))
-    print(integrate.error)
+    # Build an Integrate object
+    integral = Integrate(double_gaussian)
+
+    # Calculate the integral
+    result = integral.double_integral([[-500, 500], [-500, 500]], precision=3)
+
+    # Show the result and the accuracy
+    print("The result is", result)
+
+    # Calculate the error range
+    error_range = float(integral.error.split()[-1]) - float(integral.error.split()[0])
+    print(f"\nThe accuracy of this result is\n{integral.error} which gives us an error range of +- {error_range}")
